49c41af76b20d33fe7037a2ad7561569fe7b4ca6
GyeongeunHyun <gehyun@osstem.com>
2023-05-02 11:14:33 +0900

    공제합계, 실수령액 추가

--- lib/salary.ts
+++ lib/salary.ts
@@ -26,7 +26,7 @@ export class Salary {
   RATE = {
     국민연금: 4.5,
     건강보험: 3.545,
-    장기요양보험: 12.81, // 건강보험 기준
+    장기요양: 12.81, // 건강보험 기준
     고용보험: 0.9,
     지방세: 10, // 소득세 기준
   };
@@ -55,8 +55,8 @@ export class Salary {
   get 건강보험() {
     return this.calculate(this.monthly, this.RATE.건강보험);
   }
-  get 장기요양보험() {
-    return this.calculate(this.건강보험, this.RATE.장기요양보험);
+  get 장기요양() {
+    return this.calculate(this.건강보험, this.RATE.장기요양);
   }
   get 고용보험() {
     return this.calculate(this.monthly, this.RATE.고용보험);
@@ -88,18 +88,30 @@ export class Salary {
         .map((e) => this.calculate(e + getCb(this.monthly)));
     }
     if (this.monthly === 10_000_000) {
-      return incomeTax.at(-1)?.slice(2).map(e => this.calculate(e));
+      return incomeTax
+        .at(-1)
+        ?.slice(2)
+        .map((e) => this.calculate(e));
     }
     for (let i = 0; i < incomeTax.length - 1; i++) {
       const stdMonthly = (incomeTax[i][0] as number) * 1000;
-      if (this.monthly >= stdMonthly) {
-        return incomeTax[i].slice(2).map(e => this.calculate(e));
+      if (this.monthly < stdMonthly) {
+        return incomeTax[i + 1].slice(2).map((e) => this.calculate(e));
       }
     }
     return [];
   }
   get 지방세() {
     return this.소득세?.map((e) => this.calculate(e, this.RATE.지방세));
+  }
+  get 공제합계() {
+    const tmpTotal =
+      this.국민연금 + this.건강보험 + this.장기요양 + this.고용보험;
+
+    return this.소득세?.map((e, i) => tmpTotal + e + (this.지방세?.[i] ?? 0));
+  }
+  get 실수령액() {
+    return this.공제합계?.map((e) => this.monthly - e);
   }
 }
 
@@ -110,9 +122,11 @@ export function getSalaries() {
     return {
       연봉: s.numFormat(s.연봉),
       월급: s.numFormat(s.월급),
+      실수령액: s.실수령액?.map((e) => s.numFormat(e)),
+      공제합계: s.공제합계?.map((e) => s.numFormat(e)),
       국민연금: s.numFormat(s.국민연금),
       건강보험: s.numFormat(s.건강보험),
-      장기요양보험: s.numFormat(s.장기요양보험),
+      장기요양: s.numFormat(s.장기요양),
       고용보험: s.numFormat(s.고용보험),
       소득세: s.소득세?.map((e) => s.numFormat(e)),
       지방세: s.지방세?.map((e) => s.numFormat(e)),
