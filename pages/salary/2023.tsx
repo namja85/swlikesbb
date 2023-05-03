7531ee812db146e0fbbedc244ebb232f21612db4
GyeongeunHyun <gehyun@osstem.com>
2023-05-03 17:48:23 +0900

    adsense script 추가

--- pages/salary/2023.tsx
+++ pages/salary/2023.tsx
@@ -1,6 +1,7 @@
 import { getSalaries, Salary } from "@/lib/salary";
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import Script from "next/script";
 import React, { useMemo, useState } from "react";
 
 interface Props {
@@ -29,26 +30,42 @@ export default function Salary2023({ salaries }: Props) {
           name="description"
           content="2023년도 실수령액 정보와 각 월소득 별 4대보험 예상공제액을 확인 할 수 있습니다."
         />
+        <meta
+          name="keywords"
+          content="2023년,2024년,연봉계산,월급계산,연봉탐색,급여계산,급여탐색,실수령액,연봉,월급,실수령액,월급계산,소득수준,연봉실수령액계산,월급실수령액계산,실수령액계산,국민연금,건강보험,장기요양,고용보험,4대보험,소득세,지방소득세,공제액"
+        />
 
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:title" content="2023 실수령액"></meta>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="2023 실수령액" />
         <meta
           property="og:description"
           content="2023년도 실수령액을 확인 할 수 있습니다."
-        ></meta>
+        />
         {/* <meta
           property="og:image"
           content="https://www.pnja.or.kr/icons/thumbnail.png"
-        ></meta>
-        <meta property="og:url" content="https://www.pnja.or.kr"></meta> */}
+        ></meta> */}
+        <meta property="og:url" content="https://www.swlikesbb.com" />
       </Head>
+
+      <Script
+        id="adsense-id"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1510883278242479"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        async
+      />
 
       <div className="flex justify-between items-end">
         <h1 className="text-3xl font-bold">2023 실수령액 💰</h1>
 
         <div className="space-x-2">
           <label>부양가족 (본인포함)</label>
-          <select className="focus:outline-none" value={familyCount} onChange={changeFamilyCount}>
+          <select
+            className="focus:outline-none"
+            value={familyCount}
+            onChange={changeFamilyCount}
+          >
             {Array.from({ length: 11 }, (_, i) => (
               <option key={i}>{i + 1}</option>
             ))}
